09c8e527aacc72b729d00c9bf7f15328ded5edc6
Nicholas Patel <np4795@yahoo.com>
2020-02-13 22:45:12 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -26,7 +26,7 @@ def speak_to_grandma(phrase)
       return "HUH?! SPEAK UP, SONNY!"
     elsif phrase == phrase.upcase
       return "NO, NOT SINCE 1938!"
-    # NO, NOT SINCE 1938!
+    
     else
   		return "NO, NOT SINCE 1938!"
     end
